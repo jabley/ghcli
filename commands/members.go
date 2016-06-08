96573aaedb85186a3ee13130b0c7307e88354012
James Abley <james.abley@gmail.com>
2016-06-08 11:58:05 +0100

    Add ability to remove user from an organisation

--- commands/members.go
+++ commands/members.go
@@ -36,6 +36,22 @@ var (
 `,
 	}
 
+	cmdRemoveMember = &Command{
+		Key:   "remove",
+		Run:   removeMember,
+		Usage: "members remove -o ORGANISATION -u USERNAME",
+		Long: `Remove a member from the organisation
+
+## Options:
+	-o, --org <ORGANISATION>
+        The organisation name
+
+    -u, --user <USERNAME>
+    	The user name
+
+`,
+	}
+
 	flagMemberOrganisation,
 	flagMemberUser string
 )
@@ -46,7 +62,11 @@ func init() {
 	cmdAddMember.Flag.StringVarP(&flagMemberOrganisation, "organisation", "o", "", "ORGANISATION")
 	cmdAddMember.Flag.StringVarP(&flagMemberUser, "user", "u", "", "USER")
 
+	cmdRemoveMember.Flag.StringVarP(&flagMemberOrganisation, "organisation", "o", "", "ORGANISATION")
+	cmdRemoveMember.Flag.StringVarP(&flagMemberUser, "user", "u", "", "USER")
+
 	cmdMembers.Use(cmdAddMember)
+	cmdMembers.Use(cmdRemoveMember)
 	CmdRunner.Use(cmdMembers)
 }
 
@@ -106,6 +126,27 @@ func addMember(client *github.Client, cmd *Command, args *Args) {
 		utils.Check(err)
 
 		ui.Println(fmt.Sprintf("Added user %s to organisation %s", user, org))
+	}
+}
+
+func removeMember(client *github.Client, cmd *Command, args *Args) {
+	org, err := GetOrg(flagMemberOrganisation)
+	utils.Check(err)
+
+	user, err := getUser(flagMemberUser)
+	utils.Check(err)
+
+	if args.Noop {
+		ui.Println(fmt.Sprintf("Removing user %s from organisation %s", user, org))
+	} else {
+		utils.CheckClient(client)
+
+		res, err := client.Organizations.RemoveOrgMembership(user, org)
+
+		defer HttpCleanup(res)
+		utils.Check(err)
+
+		ui.Println(fmt.Sprintf("Removed user %s from organisation %s", user, org))
 	}
 }
 
